0e0046abe62488ea3131f9194b865c73a77868fc
John Doe <1207795882@qq.com>
2016-11-21 14:45:54 +0800

    取消AsyncTask

--- app/src/main/java/com/example/administrator/asynctasktest/ProgressActivity.java
+++ app/src/main/java/com/example/administrator/asynctasktest/ProgressActivity.java
@@ -7,14 +7,25 @@ import android.widget.ProgressBar;
 
 public class ProgressActivity extends Activity {
     private ProgressBar pg;
-
+    Progressbar pro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
         pg = (ProgressBar) findViewById(R.id.pg);
-        Progressbar pro = new Progressbar();
+        pro = new Progressbar();
         pro.execute();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        if (pro != null &&
+                pro.getStatus() == AsyncTask.Status.RUNNING) {
+            //cancel只是将对应的AsyncTask标记为cancel状态，并不是真正的取消线程的执行。
+            pro.cancel(true);
+        }
     }
 
     class Progressbar extends AsyncTask<Void, Integer, Void> {
@@ -22,6 +33,9 @@ public class ProgressActivity extends Activity {
         @Override
         protected Void doInBackground(Void... params) {
             for (int i = 0; i < 100; i++) {
+                if (isCancelled()) {
+                    break;
+                }
                 publishProgress(i);
                 try {
                     Thread.sleep(50);
@@ -35,6 +49,9 @@ public class ProgressActivity extends Activity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
+            if (isCancelled()) {
+                return;
+            }
             pg.setProgress(values[0]);
         }
     }
